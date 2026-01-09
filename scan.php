bde66eabceb47450d8ca0b662d7bdc4a11f87167
Nauman Shahid <me@nauman.cc>
2026-01-09 16:50:59 +0400

    Update scan.php
    It now scans the files inside the folders so that the "app" is aware of the "Inventory".

--- scan.php
+++ scan.php
@@ -1,18 +1,24 @@
 <?php
 header('Content-Type: application/json');
 
-$directories = array();
+$chatData = [];
 $items = scandir('.');
 
-foreach ($items as $item) {
-    if ($item === '.' || $item === '..' || !is_dir($item)) {
+foreach ($items as $folder) {
+    if ($folder === '.' || $folder === '..' || !is_dir($folder)) {
         continue;
     }
     
-    if (file_exists($item . '/_chat.txt')) {
-        $directories[] = $item;
+    if (file_exists($folder . '/_chat.txt')) {
+        $files = scandir($folder);
+        $cleanFiles = array_values(array_diff($files, ['.', '..']));
+        
+        $chatData[] = [
+            'id' => $folder,
+            'files' => $cleanFiles
+        ];
     }
 }
 
-echo json_encode($directories);
+echo json_encode($chatData);
 ?>
